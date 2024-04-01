c3e2f1a811d430ea0d9ac566173b6c923b1bd4f6
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2024-04-01 11:48:01 +0100

    Delete extra checkbox test

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -34,11 +34,6 @@ function SignIn() {
                         
                         <label for="policy" className="policy">Agree to terms and conditions</label>
                     </div>}
-                    <div className='checkbox'>
-                        <Input type="checkbox" id="policy" /> 
-                        
-                        <label for="policy" className="policy">Agree to terms and conditions</label>
-                    </div>
                     <button type="submit" className={`btn-signin ${access === "signup" ? "mb-35" : ""}`}>{access === "signin" ? "Sign In" : "Sign Up"}</button>
                     {access === "signin" && <p style={{textAlign: "center"}} className='forgot'><Link style={{textDecoration: "none", color: "#000"}} to="/reset">Forgot your password?</Link></p> }
                 </form>
